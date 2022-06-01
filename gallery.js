f4f5efed98dbcae44796f22a89a014516eaad38e
Natalie <natalie_andersen@icloud.com>
2022-06-01 10:53:52 +0200

    Forbundet galleri etc

--- gallery.js
+++ gallery.js
@@ -1,5 +1,6 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+// showSlides(slideIndex);
+
 
 // Next/previous controls
 function plusSlides(n) {
@@ -11,18 +12,19 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  // let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  // dots[slideIndex-1].className += " active";
 } 
--- product.js
+++ product.js
@@ -13,25 +13,28 @@ fetch("https://kea21-ed2b.restdb.io/rest/paintings/" + id, {
 function showProduct(product) {
   console.log(product);
 
-  document.querySelector(
-    "p.title"
-  ) .textContent = product.title
+  document.querySelector("p.title").textContent = product.title;
 
-  document.querySelector(
-    "p.size"
-  ) .textContent = product.size
+  document.querySelector("p.size").textContent = product.size;
 
-  document.querySelector(
-    "p.materials"
-  ) .textContent = product.materials
+  document.querySelector("p.materials").textContent = product.materials;
 
-  document.querySelector(
-    "p.info"
-  ) .textContent = product.info
+  document.querySelector("p.info").textContent = product.info;
 
-  document.querySelector(
-    "p.price"
-  ) .textContent = product.prize + " kr."
+  document.querySelector("p.price").textContent = product.prize + " kr.";
 
+  document.querySelector(".mySlides img").src = product.photo;
+
+  product.images.forEach(showImage);
+
+  function showImage(image) {
+    const template = document.querySelector("template").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector("img").src = image;
+
+    const parent = document.querySelector(".slideshow-container");
+    parent.appendChild(copy);
+  }
+
+  showSlides(slideIndex);
 }
-
